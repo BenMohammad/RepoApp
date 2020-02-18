f92a4e974213cc31e6e3757126b400233939ec22
Ben Mohammad <benmohammad593@gmail.com>
2020-02-18 16:54:35 +0000

    further work on data package...

--- app/src/main/java/com/benmohammad/repoapp/data/DataRepository.java
+++ app/src/main/java/com/benmohammad/repoapp/data/DataRepository.java
@@ -3,20 +3,35 @@ package com.benmohammad.repoapp.data;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.ColorSpace;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.benmohammad.repoapp.data.database.DaoCachedProjects;
 import com.benmohammad.repoapp.data.database.DaoSavedProjects;
 import com.benmohammad.repoapp.data.database.GitHubProjectsDatabase;
+import com.benmohammad.repoapp.data.database.ModelCachedGitHubProject;
+import com.benmohammad.repoapp.data.database.ModelSavedGitHubProject;
 import com.benmohammad.repoapp.data.webservice.apiresponse.GitHubClientService;
+import com.benmohammad.repoapp.data.webservice.apiresponse.GitHubRepo;
+import com.benmohammad.repoapp.data.webservice.apiresponse.items.Item;
 import com.benmohammad.repoapp.utils.Configuration;
 import com.benmohammad.repoapp.utils.WebServiceMessage;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DataRepository {
 
@@ -58,7 +73,7 @@ public class DataRepository {
         return sharedPreferences.getString("lastSearchTerm", GitHubClientService.DEFAULT_SEARCH_TERM);
     }
 
-    private void setLastSearchTerm(String searchTerm) {
+    private void saveLastSearchTerm(String searchTerm) {
         SharedPreferences.Editor myEditor = sharedPreferences.edit();
         myEditor.putString("lastSearchTerm", searchTerm);
         myEditor.apply();
@@ -88,7 +103,119 @@ public class DataRepository {
 
     public void updateFromWebservice(final String searchTerm, final int pageNumber, int resultsPerPage) {
         webServiceMessageCallStatus.setValue(WebServiceMessage.UPDATING_STATUS);
+        final List<ModelCachedGitHubProject> gitHubProjectsList =  new ArrayList<>();
+        if(retrofit == null || gitHubClient == null) {
+            retrofit = new Retrofit.Builder().baseUrl(GitHubClientService.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            gitHubClient = retrofit.create(GitHubClientService.class);
+        }
+        Call<GitHubRepo> call = gitHubClient.getRepos(searchTerm, pageNumber, resultsPerPage);
+        call.enqueue(new Callback<GitHubRepo>() {
+            @Override
+            public void onResponse(Call<GitHubRepo> call, Response<GitHubRepo> response) {
+                ArrayList<Item> itemsList = response.body().getItems();
+                for(int i = 0; i < itemsList.size(); i++) {
+                    ModelCachedGitHubProject currentProject = new ModelCachedGitHubProject();
+                    Item responseItem = itemsList.get(i);
+                    currentProject.setOwnerName(responseItem.getOwner().getLogin());
+                    currentProject.setRepoName(responseItem.getName());
+                    currentProject.setRepoSize(responseItem.getSize());
+                    currentProject.setHasWiki(responseItem.isHas_wiki());
+                    currentProject.setCreatedAt(responseItem.getCreated_at());
+                    currentProject.setPushedAt(responseItem.getPushed_at());
+                    currentProject.setUpdatedAt(responseItem.getUpdated_at());
+                    currentProject.setHtmlUrl(responseItem.getHtml_url());
+                    currentProject.setAvatarUrl(responseItem.getHtml_url());
+                    currentProject.setLanguage(responseItem.getLanguage());
+                    currentProject.setForksCount(responseItem.getForks_count());
+                    currentProject.setScore(responseItem.getScore());
+                    currentProject.setDescription(responseItem.getDescription());
+                    gitHubProjectsList.add(currentProject);
+                }
 
+                if(!gitHubProjectsList.isEmpty()) {
+                    boolean clearPreviousCache;
+                    if(pageNumber == 1) {
+                        clearPreviousCache = true;
+                        saveLastSearchTerm(searchTerm);
+
+                    } else {
+                        clearPreviousCache = false;
+                    }
+
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GitHubRepo> call, Throwable t) {
+                Log.e("Error", "This is wrong....");
+            }
+        });
     }
+
+
+    public LiveData<List<ModelCachedGitHubProject>> getAllCachedProjects() {
+        return cachedProjectsDao.getAllCachedFromDb();
+    }
+
+    public LiveData<List<ModelSavedGitHubProject>> getAllSavedProjects() {
+        return savedProjectsDao.getAllSavedProjects();
+    }
+
+    public MutableLiveData<WebServiceMessage> getWebServiceMessage() {
+        return webServiceMessageCallStatus;
+    }
+
+    public void clearWebServiceMessage() {
+        webServiceMessageCallStatus = new MutableLiveData<>();
+    }
+
+    private void cacheProjectsList(List<ModelCachedGitHubProject> cachedGitHubProjects, boolean clearCache) {
+        new SaveListToCacheAsyncTask(cachedProjectsDao, clearCache).execute(cachedGitHubProjects);
+    }
+
+    public void trimCacheTable() {
+        new TrimCacheAsyncTask(cachedProjectsDao).execute();
+    }
+
+    private static class SaveListToCacheAsyncTask extends AsyncTask<List<ModelCachedGitHubProject>, Void, Void> {
+        private DaoCachedProjects cachedProjectsDao;
+        boolean clearCacheBeforeAdding;
+
+        private SaveListToCacheAsyncTask(DaoCachedProjects cachedProjectsDao, boolean clearCacheBeforeAdding) {
+            this.cachedProjectsDao = cachedProjectsDao;
+            this.clearCacheBeforeAdding = clearCacheBeforeAdding;
+        }
+
+        @Override
+        protected Void doInBackground(List<ModelCachedGitHubProject>... lists) {
+            if(clearCacheBeforeAdding) {
+                cachedProjectsDao.deleteAllCachedRepos();
+            }
+            cachedProjectsDao.saveToCache(lists[0]);
+            return null;
+        }
+    }
+
+    private static class TrimCacheAsyncTask extends AsyncTask<ModelCachedGitHubProject, Void, Void> {
+        private DaoCachedProjects  cachedProjectsDao;
+
+        public TrimCacheAsyncTask(DaoCachedProjects cachedProjectsDao) {
+            this.cachedProjectsDao = cachedProjectsDao;
+        }
+
+        @Override
+        protected Void doInBackground(ModelCachedGitHubProject... modelCachedGitHubProjects) {
+            cachedProjectsDao.trimCacheTable(GitHubClientService.RESULTS_PER_PAGE);
+            return null;
+        }
+    }
+
+
+
+
 
 }
